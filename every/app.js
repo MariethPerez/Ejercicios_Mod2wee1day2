30d31a543077aa17607ffa7cf83847afa0b94458
Marieth <mperezzevallos@gmail.com>
2019-05-16 12:38:42 -0500

    Add methods every and some

--- every/app.js
+++ every/app.js
@@ -1,5 +1,17 @@
 function everyfake(array, func) {
   var result = true;
+  for (var i = 0; i < array.lenght; i++) {
+    //result = result && func(array[i]);
+    result = result && func(array[i]);
+  }
+  return result;
+}
+var a = [1, 2, 3];
+//document.getElementById("demo").innerHTML = fakepop(a);
+console.log(everyfake(a, element => element > 50));
+
+function everyfake(array, func) {
+  var result = true;
   for (var i = 0; i < array.length; i++) {
     result = result && func(array[i]);
   }
@@ -7,8 +19,5 @@ function everyfake(array, func) {
 }
 
 var a = [1, 2, 3];
-document.getElementById("demo").innerHTML = everyfake(
-  a,
-  element => element > 50
-);
+//result = every(a, element => element > 50);
 console.log(everyfake(a, element => element > 50)); // false
--- some/app.js
+++ some/app.js
@@ -1,5 +1,17 @@
 function somefake(array, func) {
   var result = false;
+  for (var i = 0; i < array.lenght; i++) {
+    //result = result && func(array[i]);
+    result = result || func(array[i]);
+  }
+  return result;
+}
+var a = [1, 2, 4, 5, 6];
+//document.getElementById("demo").innerHTML = fakepop(a);
+console.log(everyfake(a, element => element > 3));
+
+function somefake(array, func) {
+  var result = false;
   for (var i = 0; i < array.length; i++) {
     result = result || func(array[i]);
   }
@@ -7,5 +19,5 @@ function somefake(array, func) {
 }
 
 var a = [1, 2, 4, 5, 6];
-document.getElementById("demo").innerHTML = somefake(a, element => element > 3);
+//result = every(a, element => element > 50);
 console.log(somefake(a, element => element > 3)); // true
